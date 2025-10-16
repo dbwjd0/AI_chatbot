15e309f02b0a5c7ea8152ed7fe6ff2ab6012c862
카레 <dbwjd1895@naver.com>
2025-10-16 15:48:19 +0900

    사용자 일정표 추가

--- chatbot_app/urls.py
+++ chatbot_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import main, chatWithAi, auth
+from .views import main, chatWithAi, auth, schedule
 
 urlpatterns = [
     path('', main.room, name='room'),
@@ -11,4 +11,5 @@ urlpatterns = [
     path('login/', auth.login_view, name='login'),
     path('logout/', auth.logout_view, name='logout'),
     path('ai_status/', main.ai_status, name='ai_status'),
+    path('schedule/', schedule.schedule_view, name='schedule'),
 ]
